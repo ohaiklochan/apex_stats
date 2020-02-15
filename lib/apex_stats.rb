a793d3f29abb8a77acd649761918536b91d763a5
Chloe Foronda <caforonda103@yahoo.com>
2020-02-15 15:57:21 +0000

    modified files

--- lib/apex_stats.rb
+++ lib/apex_stats.rb
@@ -1,2 +1,9 @@
 require_relative "./apex_stats/version"
 require_relative './apex_stats/cli'
+
+
+module ApexStats
+  def self.hello_world
+    "Good morning world and all who inhabit it!"
+  end
+end
