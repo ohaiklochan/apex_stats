476c9de6d05c22984bf2aa7d3d8ed871226a8c68
Chloe Foronda <caforonda103@yahoo.com>
2020-02-17 01:52:45 +0000

    modified files

--- lib/apex_stats/legends.rb
+++ lib/apex_stats/legends.rb
@@ -5,66 +5,66 @@ class ApexStats::Legends
   @@all = []
     
   def self.scrape_legend
-    doc = Nokogiri::HTML(open("https://dreamteam.gg/apex/wiki/apex-legends-complete-characters-guide"))
+    doc = Nokogiri::HTML(open("https://apexlegends.gamepedia.com/wraith"))
     legend = ApexStats::Legends.new
               binding.pry
-    legend.name = doc.search("h2.text-align i").text
-    legend.backstory = doc.search("p.span strong").text.gsub(".",". ")
+    legend.name = doc.search("h1#firstHeading.firstHeading").text
+    legend.backstory = doc.search("#mw-content-text p")[3..5].text.gsub(".",". ")
     legend.abilities = []
     doc.search("https://dreamteam.gg/apex/wiki/apex-legends-complete-characters-guide").each do |ability|
-      passive_ability_name = passiive_ability.search("").text
-      passive_ability_description = passiive_ability.search("").text
-      tactical_ability_name = tactical_ability.search("").text
-      tactical_ability_description = tactical_ability.search("").text
-      ultimate_ability_name = ultimate_ability.search("").text
-      ultimate_ability_description = ultimate_ability.search("").text
-      legend.abilities << {:passive_ability_name => passive_ability_name, :passive_ability_description => passive_ability_description, :tactical_ability_name => tactical_ability_name, :tactical_ability_description => tactical_ability_description,:ultimate_ability_name => ultimate_ability_name, :ultimate_ability_description => ultimate_ability_description}
+      passive_ability.name = doc.search("").text
+      passive_ability.description = doc.search("").text
+      tactical_ability.name = doc.search("").text
+      tactical_ability.description = doc.search("").text
+      ultimate_ability.name = doc.search("").text
+      ultimate_ability.description = doc.search("").text
+      legend.abilities << {:passiveabilityname => passive_ability_name, :passiveabilitydescription => passive_ability_description, :tacticalabilityname => tactical_ability_name, :tacticalabilitydescription => tactical_ability_description,:ultimateabilityname => ultimate_ability_name, :ultimateabilitydescription => ultimate_ability_description}
     end
     legend
   end
   
   def self.wraith
-    wraith = self.scrape_legend("https://play-apex.net/stats/Wraith")
+    wraith = self.scrape_legend("https://apexlegends.gamepedia.com/Wraith")
     wraith
   end
   
   def self.pathfinder
-    pathfinder = self.scrape_legend("https://play-apex.net/stats/Pathfinder")
+    pathfinder = self.scrape_legend("https://apexlegends.gamepedia.com/pathfinder")
     pathfinder
   end
   
   def self.lifeline
-    lifeline = self.scrape_legend("https://play-apex.net/stats/Lifeline")
+    lifeline = self.scrape_legend("https://apexlegends.gamepedia.com/lifeline")
     lifeline
   end
   
   def self.bangalore
-    bangalore = self.scrape_legend("https://play-apex.net/stats/Bangalore")
+    bangalore = self.scrape_legend("https://apexlegends.gamepedia.com/bangalore")
     bangalore
   end
   
   def self.octane
-    octane = self.scrape_legend("https://play-apex.net/stats/Octane")
+    octane = self.scrape_legend("https://apexlegends.gamepedia.com/octane")
     octane
   end
   
   def self.bloodhound
-    bloodhound = self.scrape_legend("https://play-apex.net/stats/Bloodhound")
+    bloodhound = self.scrape_legend("https://apexlegends.gamepedia.com/bloodhound")
     bloodhound
   end
   
   def self.mirage
-    mirage = self.scrape_legend("https://play-apex.net/stats/Mirage")
+    mirage = self.scrape_legend("https://apexlegends.gamepedia.com/mirage")
     mirage
   end
   
   def self.gibraltar
-    gibraltar = self.scrape_legend("https://play-apex.net/stats/Gibraltar")
+    gibraltar = self.scrape_legend("https://apexlegends.gamepedia.com/gibraltar")
     gibraltar
   end
   
   def self.caustic
-    caustic = self.scrape_legend("https://play-apex.net/stats/Caustic")
+    caustic = self.scrape_legend("https://apexlegends.gamepedia.com/caustic")
     caustic
   end
   
